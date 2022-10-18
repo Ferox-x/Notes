eba22d2efe134b8b071c4fbf9d0a813c837e608c
Egor Malov <egor.malov2014@gmail.com>
2022-10-18 19:26:29 +0700

    initial commit

---
+++ app_notes/config/settings.py
@@ -0,0 +1,106 @@
+from os import path
+from pathlib import Path
+
+from dotenv import dotenv_values
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+config_env = dotenv_values(path.join(BASE_DIR, '.env'))
+
+SECRET_KEY = config_env.get('SECRET_KEY')
+
+DEBUG = True
+
+ALLOWED_HOSTS = config_env.get('ALLOWED_HOSTS', 'localhost').split()
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'debug_toolbar',
+    'rest_framework',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'debug_toolbar.middleware.DebugToolbarMiddleware'
+]
+
+ROOT_URLCONF = 'config.urls'
+
+TEMPLATES_DIR = path.join(BASE_DIR.parent, 'templates')
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [TEMPLATES_DIR],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'config.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config_env.get('NAME_DB'),
+        'USER': config_env.get('USER_DB'),
+        'PASSWORD': config_env.get('PASSWORD_DB'),
+        'HOST': config_env.get('HOST_DB'),
+        'PORT': config_env.get('PORT_DB'),
+    }
+}
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+STATIC_URL = 'static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = path.join(BASE_DIR.parent, 'media')
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
