f08ebdd550ceec7842001b8dcb1248b2a2e24c6f
Egor Malov <egor.malov2014@gmail.com>
2022-11-06 04:00:40 +0700

    add react

--- app_notes/config/urls.py
+++ app_notes/config/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('users/', include('users.urls')),
-    path('', include('frontend_django.urls')),
+    path('', include('frontend.urls')),
 
     path('__debug__/', include('debug_toolbar.urls')),
 
