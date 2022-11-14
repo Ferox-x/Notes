0162fd0ec8a46ac6bda8cd109747dbbec599a26c
Egor Malov <egor.malov2014@gmail.com>
2022-11-14 22:26:40 +0700

    commit

--- app_notes/frontend/src/index.js
+++
@@ -1,7 +0,0 @@
-import App from "./components/App";
-import {createRoot} from "react-dom/client";
-
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement)
-root.render(<App/>);
