5cbbd80ec6495d9ab6814048ddf0912ee8b3bba4
cantchoosecolor <yulya_konokhova@mail.ru>
2022-11-14 22:25:44 +0700

    react-bag

--- app_notes/frontend/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from frontend.views import Index
-
-urlpatterns = [
-    path('', Index.as_view(), name='index'),
-]
---
+++ frontend/src/components/layouts/main_page/main_page.jsx
@@ -0,0 +1,16 @@
+import {Component} from "react";
+import './main_page.module.css'
+
+class MainPage extends Component {
+  render() {
+    return (
+      <section className="page-title">
+        <p className="page-title__text">Главная</p>
+      </section>
+    )
+  }
+}
+
+export default MainPage;
+
+
